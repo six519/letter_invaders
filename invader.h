954ed701c24753861803d69113ea170367c574f0
six519 <ferdinandsilva@ferdinandsilva.com>
2024-01-15 10:04:34 +0800

    add more letter

--- invader.h
+++ invader.h
@@ -1,13 +1,36 @@
 #ifndef _INVADER_H_
 #define _INVADER_H_
 
-#define AVAILABLE_LETTERS_COUNT 27
+#define AVAILABLE_LETTERS_COUNT 76
 #define LETTER_MAX_LENGTH 10
 
 static char AVAILABLE_LETTERS[AVAILABLE_LETTERS_COUNT][LETTER_MAX_LENGTH] = {
-    "a",
-    "b",
-    "c",
+    "a", "A",
+    "b", "B",
+    "c", "C",
+    "d", "D",
+    "e", "E",
+    "f", "F",
+    "g", "G",
+    "h", "H",
+    "i", "I",
+    "j", "J",
+    "k", "K",
+    "l", "L",
+    "m", "M",
+    "n", "N",
+    "o", "O",
+    "p", "P",
+    "q", "Q",
+    "r", "R",
+    "s", "S",
+    "t", "T",
+    "u", "U",
+    "v", "V",
+    "w", "W",
+    "x", "X",
+    "y", "Y",
+    "z", "Z",
     "Ja"
     "sal",
     "lall;",
